a276958a5add4bca22bfe96956b148feee13584f
Øyvind Rønne <oyron@equinor.com>
2021-01-09 09:32:45 +0100

    use exiftool for reading metadata

--- imgsort.py
+++ imgsort.py
@@ -1,52 +1,23 @@
-import exifread, pyheif, io, re, sys
 from pathlib import Path
-from datetime import datetime
 from PIL import Image
+from datetime import datetime
+
+import exiftool, sys, re
 
 def parse_exif_date(date_str):
     match = re.search("(\d+):(\d+):(\d+) (\d+):(\d+):(\d+)", date_str)
     return datetime(int(match.group(1)), int(match.group(2)), int(match.group(3)), int(match.group(4)), int(match.group(5)), int(match.group(6)))
 
-
-def get_date_from_heic(file_name):
-    try:
-        heif_file = pyheif.read_heif(file_name)
-
-    except pyheif.error.HeifError as err:
-        print(err)
-        return None
-
-    for metadata in heif_file.metadata:
-        file_stream = io.BytesIO(metadata['data'][6:])
-        tags = exifread.process_file(file_stream, details=False)
-        date_str = str(tags.get("EXIF DateTimeOriginal"))
-        match = re.search("(\d+):(\d+):(\d+) (\d+):(\d+):(\d+)", date_str)
-        if match:
-            return datetime(int(match.group(1)), int(match.group(2)), int(match.group(3)), int(match.group(4)), int(match.group(5)), int(match.group(6)))
-
-def get_date_from_jpg(file_name):
-    try:
-        image = Image.open(file_name)
-        exifdata = image._getexif()
-
-        if exifdata:
-            date_str = exifdata.get(36867)
-            if (date_str):
-                return parse_exif_date(date_str)
-
-    except IOError as err:
-        print(err)
-
-def extension(file_name):
-    match = re.search("\w+\.(\w+)", file_name)
-    if match and match.group(1):
-        return match.group(1)
-
-def dir_name(date):
-    return 
+def get_date(f):
+    tag_names = {'*': 'EXIF:DateTimeOriginal', '.mov': 'QuickTime:CreateDate'}
+    tag = tag_names[f.suffix.lower()] if f.suffix.lower() in tag_names else tag_names['*']
+    with exiftool.ExifTool() as et:
+        metadata = et.get_metadata(str(f.absolute()))
+        date_str = metadata[tag]
+        return parse_exif_date(date_str)
 
 def get_target_name(f):
-    date = get_date_from_jpg(f) if f.suffix.lower() == ".jpg" else get_date_from_heic(f)
+    date = get_date(f)
     if not date:
         print("Could not extract date from " + f.name)
         return None
@@ -59,14 +30,14 @@ def move_file(f, target):
     new_f = target.joinpath(f.name)
     f.rename(new_f)
 
-
 def run(base_path = "."):
-    extensions = [".jpg", ".heic"]
+    extensions = [".jpg", ".heic", ".mov"]
     basepath = Path(base_path)
     files = basepath.iterdir()
     for f in files:
         print("Processing " + f.name)
         if not f.is_file():
+            print("Ignoring directory " + f.name)
             continue
         if not f.suffix.lower() in extensions:
             print("Unsupported file type: " + f.name)
@@ -76,12 +47,9 @@ def run(base_path = "."):
         if dir_name:
             target = basepath.joinpath(dir_name)
             move_file(f, target)
-
-        #print(f.name + ": "  + dir_name + ": " + str(target.exists()))
         
             
 if __name__ == "__main__":
-    print(sys.argv)
     if len(sys.argv) > 1:
         run(sys.argv[1])
     else:
